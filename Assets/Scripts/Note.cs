e6aa9768f0d7d5202957b171e8ab5b477ddf5242
OzoneY1911 <121692316+OzoneY1911@users.noreply.github.com>
2023-09-08 15:01:56 +0200

    Updated Note prefab and script

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
+
     [Header("Position")]
     [SerializeField] Vector2 startPoint;
     [SerializeField] Vector2 endPoint;
-    [SerializeField] float shiftStartPointX; 
-    
-    [Header("Speed")]
-    [SerializeField] float noteSpeed = 5;
+    [SerializeField] float shiftStartPointX;
 
-    void Awake()
+    [Header("Speed")]
+    [SerializeField] float noteSpeed = 5f;
+
+    [Header("Note Size")]
+    [SerializeField] float noteSize = 1f;
+
+    void OnEnable()
     {
+        transform.localScale = new Vector3(noteSize, noteSize, noteSize);
+
         startPoint.x -= shiftStartPointX;
         transform.position = startPoint;
     }
@@ -21,12 +27,26 @@ public class Note : MonoBehaviour
     void Update()
     {
         // Move between two points
-        transform.position = Vector3.MoveTowards(transform.position, endPoint, noteSpeed * Time.deltaTime);
+        if (gameObject.activeSelf)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPoint, noteSpeed * Time.deltaTime);
+        }
 
         // If Note reaches endPoint
         if (transform.position == new Vector3(endPoint.x, endPoint.y, 0))
         {
-            
+            gameObject.SetActive(false);
         }
+    }
+
+    public void Init(Vector2 startPoint, Vector2 endPoint, float shiftStartPointX, float noteSpeed, float noteSize)
+    {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.shiftStartPointX = shiftStartPointX;
+        this.noteSpeed = noteSpeed;
+        this.noteSize = noteSize;
+
+        OnEnable();
     }
 }
